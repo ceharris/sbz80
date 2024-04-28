011e50c899e23783094d71f0e57ee42d0eb8591a
Carl Harris <ceharris@vt.edu>
2024-04-28 05:55:33 -0400

    whitespace cleanup

--- mk1/acia.h.asm
+++ mk1/acia.h.asm
@@ -1,9 +1,9 @@
 #ifndef _ACIA_H
 #define _ACIA_H
 
-                        global acia_init
-                        global acia_ioctl
-                        global acia_getc
-                        global acia_putc
+		global acia_init
+		global acia_ioctl
+		global acia_getc
+		global acia_putc
 
 #endif ; _ACIA_H
