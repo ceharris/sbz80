4c204cad3ba4a5e2ca7fb1afdee05d98a8bfd9c4
Carl Harris <ceharris@vt.edu>
2024-05-15 16:57:24 -0400

    delay isn't available via dispatcher

--- mk1/svc.h.asm
+++ mk1/svc.h.asm
@@ -6,7 +6,6 @@
 __cgetc		defl 0		; Get char from console input to A
 __cgetcnb	defl 1		; Get char from console to A, Z if none
 __cgets		defl 2		; Get string from console input into (HL)
-__cflush	defl 4		; Flush console input 
 __cputc		defl 3		; Put character C to console output 
 __cputs		defl 4		; Put string (HL) to console output
-__delay		defl 5		; Delay for BC milliseconds
+__cflush	defl 5		; Flush console input 
